30442246dd2e5fd9047a5e5d9b49b6d500edfbb6
Alistair Willis <alistairrwillis@gmail.com>
2018-04-11 19:31:25 +0100

    add quiz component

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
+import quizData from '../../data/quiz-data';
 
 class Quiz extends Component {
+    state = {
+        quiz: {}
+    }
+    componentDidMount() {
+        console.log(this.props.match.params.id);
+        const quiz = quizData.find(data => {
+            return data.id === +this.props.match.params.id;
+        }); 
+        this.setState({quiz});
 
+    }
     render() {
-        return <h1>QUIZ</h1>
+        return (
+            <h1>{this.state.quiz.title}</h1>
+        )
     }
 }
 
